987f89d8342dca18d46a05104cec64b6d42c9c02
Fatoba Olumide <kratosolomon25@gmail.com>
2026-04-06 11:17:14 +0100

    first commit2

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:8081',
   'http://localhost:3000',
+  'https://ekitistateewers.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean)
 
